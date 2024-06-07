81617a148435c8f7a40e71ca35c97c2db0f981c0
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-07 22:23:25 +0530

    COMPLETE

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Add from './components/Add';
 import Search from './components/Search';
 import Delete from './components/Delete';
 import View from './components/View';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Add/>
-      <Search/>
-      <Delete/>
-      <View/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Add/>}/>
+        <Route path='/s' element={<Search/>}/>
+        <Route path='/d' element={<Delete/>}/>
+        <Route path='/v' element={<View/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
